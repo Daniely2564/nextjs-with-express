b43d1acb2b1c37212a04e06384fa6e3b906dd4b3
unknown <jarkassp2@gmail.com>
2019-02-02 10:43:09 -0500

    Final Form

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -5,4 +5,8 @@ const request = require('superagent');
 const async = require('async');
 
 
+router.get('/test', (req, res) => {
+  res.status(200).json({ test: 'This is from the server' })
+})
+
 module.exports = router;
