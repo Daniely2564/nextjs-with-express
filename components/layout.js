3333261c2218399e9a696f0e460f0e3747e73443
unknown <jarkassp2@gmail.com>
2019-02-02 10:37:34 -0500

    Basic form

--- components/layout.js
+++ components/layout.js
@@ -1,41 +1,82 @@
 import Link from 'next/link';
 import Head from 'next/head';
 
-export default ({ children, title = 'Next.js / Express App' }) => (
+export default ({ children, title = 'Clover API' }) => (
   <div>
     <Head>
-      <title>{ title }</title>
+      <title>{title}</title>
       <meta charSet='utf-8' />
       <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='defaultLanguage' content='fr' />
       <meta name='availableLanguages' content='fr, en' />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
       <link rel="stylesheet" href="/static/styles.css" />
     </Head>
 
-    <nav className='navbar navbar-default navbar-fixed-top'>
-      <div className='container-fluid'>
-        <div className='navbar-header'>
-          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-            <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-          </button>
-          <Link href='/'><a className='navbar-brand'>Next.js Spotify</a></Link>
-        </div>
+    <nav className="site-header sticky-top py-1">
+      <div className="container d-flex flex-column flex-md-row justify-content-between">
+        <a className="py-2" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+        </a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Tour</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Product</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Features</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Support</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+        <a className="py-2 d-none d-md-inline-block" href="#">Cart</a>
       </div>
     </nav>
 
-    { children }
+    {children}
 
-    <div className='container'>
-      <footer className='footer'>
-        &copy; 2017 - mluberry
-      </footer>
-    </div>
-
+    <footer className="container py-5">
+      <div className="row">
+        <div className="col-12 col-md">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+          <small className="d-block mb-3 text-muted">&copy; 2017-2018</small>
+        </div>
+        <div className="col-6 col-md">
+          <h5>Features</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Cool stuff</a></li>
+            <li><a className="text-muted" href="#">Random feature</a></li>
+            <li><a className="text-muted" href="#">Team feature</a></li>
+            <li><a className="text-muted" href="#">Stuff for developers</a></li>
+            <li><a className="text-muted" href="#">Another one</a></li>
+            <li><a className="text-muted" href="#">Last time</a></li>
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>Resources</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Resource</a></li>
+            <li><a className="text-muted" href="#">Resource name</a></li>
+            <li><a className="text-muted" href="#">Another resource</a></li>
+            <li><a className="text-muted" href="#">Final resource</a></li>
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>Resources</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Business</a></li>
+            <li><a className="text-muted" href="#">Education</a></li>
+            <li><a className="text-muted" href="#">Government</a></li>
+            <li><a className="text-muted" href="#">Gaming</a></li>
+          </ul>
+        </div>
+        <div className="col-6 col-md">
+          <h5>About</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Team</a></li>
+            <li><a className="text-muted" href="#">Locations</a></li>
+            <li><a className="text-muted" href="#">Privacy</a></li>
+            <li><a className="text-muted" href="#">Terms</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { Component } from 'react'
 
-import Layout from '../components/layout';
-
-export default () => {
-  return (
-    <Layout title={ 'Next.js + Express' }>
-      <div className='container'>
-        <div className='jumbotron'>
-          <h1>Next.js + Express</h1>
-          <p>A simple app using Spotify API</p>
-          <p>
-            <Link href='/search'><a className='btn btn-primary btn-lg' role='button'>Use it !</a></Link>
-          </p>
-        </div>
-      </div>
-    </Layout>
-  );
+export default class Index extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Something </h1>
+            </div>
+        )
+    }
 }
